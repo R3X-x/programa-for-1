981709bb28c19bebb551d68c7f8080a4a5824b07
R3X-x <81946959+R3X-x@users.noreply.github.com>
2022-01-11 15:22:08 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,24 @@
+let nPisosBase = Number(prompt("Ingrese el número de pisos base que quiere"))
+let nPisos = Number(prompt("Ingrese el número de pisos que quiere"))
+function piso {
+    console.log("|  _     _  |")
+    console.log("| |_|   |_| |")
+    console.log("|___________|")
+}
+function pisoBase {
+    console.log("|  _     _  |")
+    console.log("| |_| _ |_| |")
+    console.log("|____|_|____|")
+}
+function techo {
+    console.log("_____________")
+}
+function base (nPisosBase){
+    for(let i=0;i<3;i++)
+}
+function casa (nPisos){
+    for(let i=0;i<3;i++)
+    techo(ancho)
+    piso(ancho)
+    pisoBase(ancho)
+}
